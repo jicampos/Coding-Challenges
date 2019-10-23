51f152ed49559ee1b80b48b67039494ab50123d7
jicampos <javic9788@gmail.com>
2019-10-23 02:41:51 -0500

    cleaned up code

--- minPathSum.java
+++ minPathSum.java
@@ -8,15 +8,12 @@ class Solution {
         
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                if(i == 0 && j == 0) {
+                if(i == 0 && j == 0)
                     perm[0][0] = grid[0][0];
-                } 
-                else if(i == 0 || j == 0) {
-                    if(i == 0)
-                        perm[i][j] = grid[i][j] + perm[i][j-1];
-                    else
-                        perm[i][j] = grid[i][j] + perm[i-1][j];
-                }
+                else if(i == 0)
+                    perm[i][j] = grid[i][j] + perm[i][j-1];
+                else if(j == 0)
+                    perm[i][j] = grid[i][j] + perm[i-1][j];
                 else { 
                     if(perm[i][j-1] > perm[i-1][j])
                         perm[i][j] = grid[i][j] + perm[i-1][j];
